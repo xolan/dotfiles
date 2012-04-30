a628b647b833ff34b121bc3e048d124f420a4e99
Jonas Svarvaa <jonassvarvaa@gmail.com>
2012-04-30 16:29:17 +0200

    Monokai tweaks. New background.

--- awesome/themes/monokai/theme.lua
+++ awesome/themes/monokai/theme.lua
@@ -1,5 +1,5 @@
 ---------------------------
--- Solarized awesome theme --
+-- Monokai awesome theme --
 ---------------------------
 
 theme = {}
@@ -7,9 +7,9 @@ theme = {}
 theme.font          = "Terminus 8"
 
 theme.bg_normal     = "#1b1d1e"
-theme.bg_focus      = "#505354"
-theme.bg_urgent     = "#ff5995"
-theme.bg_minimize   = "#f92672"
+theme.bg_focus      = "#3b3d1e"
+theme.bg_urgent     = "#fd971f"
+theme.bg_minimize   = "#56c2d6"
 
 theme.fg_normal     = "#ccccc6"
 theme.fg_focus      = "#f8f8f2"
@@ -18,7 +18,7 @@ theme.fg_minimize   = "#ffffff"
 
 theme.border_width  = "1"
 theme.border_normal = "#505454"
-theme.border_focus  = "#fd971f"
+theme.border_focus  = "#82b414"
 theme.border_marked = "#feed6c"
 
 -- There are other variable sets
@@ -32,15 +32,15 @@ theme.border_marked = "#feed6c"
 --theme.taglist_bg_focus = "#ff0000"
 
 -- Display the taglist squares
-theme.taglist_squares_sel   = "/usr/share/awesome/themes/default/taglist/squarefw.png"
-theme.taglist_squares_unsel = "/usr/share/awesome/themes/default/taglist/squarew.png"
+theme.taglist_squares_sel   = "/home/xolan/.config/awesome/themes/monokai/taglist/squarefw.png"
+theme.taglist_squares_unsel = "/home/xolan/.config/awesome/themes/monokai/taglist/squarew.png"
 
-theme.tasklist_floating_icon = "/usr/share/awesome/themes/default/tasklist/floatingw.png"
+theme.tasklist_floating_icon = "/home/xolan/.config/awesome/themes/monokai/tasklist/floatingw.png"
 
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = "/usr/share/awesome/themes/default/submenu.png"
+theme.menu_submenu_icon = "/home/xolan/.config/awesome/themes/monokai/submenu.png"
 theme.menu_height = "15"
 theme.menu_width  = "100"
 
@@ -50,45 +50,45 @@ theme.menu_width  = "100"
 --theme.bg_widget = "#cc0000"
 
 -- Define the image to load
-theme.titlebar_close_button_normal = "/usr/share/awesome/themes/default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = "/usr/share/awesome/themes/default/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = "/home/xolan/.config/awesome/themes/monokai/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = "/home/xolan/.config/awesome/themes/monokai/titlebar/close_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = "/home/xolan/.config/awesome/themes/monokai/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = "/home/xolan/.config/awesome/themes/monokai/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = "/home/xolan/.config/awesome/themes/monokai/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = "/home/xolan/.config/awesome/themes/monokai/titlebar/ontop_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = "/home/xolan/.config/awesome/themes/monokai/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = "/home/xolan/.config/awesome/themes/monokai/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = "/home/xolan/.config/awesome/themes/monokai/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = "/home/xolan/.config/awesome/themes/monokai/titlebar/sticky_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = "/home/xolan/.config/awesome/themes/monokai/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = "/home/xolan/.config/awesome/themes/monokai/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = "/home/xolan/.config/awesome/themes/monokai/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = "/home/xolan/.config/awesome/themes/monokai/titlebar/floating_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = "/home/xolan/.config/awesome/themes/monokai/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = "/home/xolan/.config/awesome/themes/monokai/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = "/home/xolan/.config/awesome/themes/monokai/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = "/home/xolan/.config/awesome/themes/monokai/titlebar/maximized_focus_active.png"
 
 -- You can use your own command to set your wallpaper
-theme.wallpaper_cmd = { "awsetbg /usr/share/awesome/themes/default/background.png" }
+theme.wallpaper_cmd = { "awsetbg /home/xolan/.config/awesome/themes/monokai/background.png" }
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
-theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairvw.png"
-theme.layout_floating  = "/usr/share/awesome/themes/default/layouts/floatingw.png"
-theme.layout_magnifier = "/usr/share/awesome/themes/default/layouts/magnifierw.png"
-theme.layout_max = "/usr/share/awesome/themes/default/layouts/maxw.png"
-theme.layout_fullscreen = "/usr/share/awesome/themes/default/layouts/fullscreenw.png"
-theme.layout_tilebottom = "/usr/share/awesome/themes/default/layouts/tilebottomw.png"
-theme.layout_tileleft   = "/usr/share/awesome/themes/default/layouts/tileleftw.png"
-theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tilew.png"
-theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
-theme.layout_spiral  = "/usr/share/awesome/themes/default/layouts/spiralw.png"
-theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindlew.png"
+theme.layout_fairh = "/home/xolan/.config/awesome/themes/monokai/layouts/fairhw.png"
+theme.layout_fairv = "/home/xolan/.config/awesome/themes/monokai/layouts/fairvw.png"
+theme.layout_floating  = "/home/xolan/.config/awesome/themes/monokai/layouts/floatingw.png"
+theme.layout_magnifier = "/home/xolan/.config/awesome/themes/monokai/layouts/magnifierw.png"
+theme.layout_max = "/home/xolan/.config/awesome/themes/monokai/layouts/maxw.png"
+theme.layout_fullscreen = "/home/xolan/.config/awesome/themes/monokai/layouts/fullscreenw.png"
+theme.layout_tilebottom = "/home/xolan/.config/awesome/themes/monokai/layouts/tilebottomw.png"
+theme.layout_tileleft   = "/home/xolan/.config/awesome/themes/monokai/layouts/tileleftw.png"
+theme.layout_tile = "/home/xolan/.config/awesome/themes/monokai/layouts/tilew.png"
+theme.layout_tiletop = "/home/xolan/.config/awesome/themes/monokai/layouts/tiletopw.png"
+theme.layout_spiral  = "/home/xolan/.config/awesome/themes/monokai/layouts/spiralw.png"
+theme.layout_dwindle = "/home/xolan/.config/awesome/themes/monokai/layouts/dwindlew.png"
 
 theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
 
