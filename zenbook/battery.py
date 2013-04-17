0917fde97a5ab2e3c2d4836b18b91e8fe5be5fbf
Jonas Svarvaa <jonassvarvaa@gmail.com>
2013-04-17 21:04:17 +0200

    Added "awesome" switch for battery.py

--- zenbook/battery.py
+++ zenbook/battery.py
@@ -5,12 +5,24 @@ from clint.textui import colored, puts
 from sh import cat
 
 def output(status, post=''):
-    if status > 75:
-        puts(colored.green(str(int(status))+post))
-    elif status > 25:
-        puts(colored.yellow(str(int(status))+post))
+    if 'awesome' in post.split(' '):
+        suffix = '</span>'
+        if status > 75:
+            prefix = '<span color="green">'
+            puts(prefix + str(int(status)) + '%' + suffix)
+        elif status > 25:
+            prefix = '<span color="yellow">'
+            puts(prefix + str(int(status)) + '%' + suffix)
+        else:
+            prefix = '<span color="red">'
+            puts(prefix + str(int(status)) + '%' + suffix)
     else:
-        puts(colored.red(str(int(status))+post))
+        if status > 75:
+            puts(colored.green(str(int(status))+post))
+        elif status > 25:
+            puts(colored.yellow(str(int(status))+post))
+        else:
+            puts(colored.red(str(int(status))+post))
 
     return 0
 
