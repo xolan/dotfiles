688bb6bc7b38a036af13fbf6a5f54f9c77969bf6
Jonas Svarvaa <jonassvarvaa@gmail.com>
2013-11-26 16:28:58 +0100

    Added work screen setup

---
+++ zenbook/work_screen.sh
@@ -0,0 +1,5 @@
+xrandr --newmode "2560x1440" 220.812 2560 2608 2640 2720 1440 1443 1448 1478 -hsync -vsync
+xrandr --addmode HDMI1 "2560x1440"
+xrandr --output HDMI1 --off
+sleep 2s
+xrandr --output HDMI1 --mode "2560x1440" --left-of eDP1  
