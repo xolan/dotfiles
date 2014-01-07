96fa30818381b5e342641f2ef13882b376034b2c
Jonas Svarvaa <jonassvarvaa@gmail.com>
2014-01-07 16:30:08 +0100

    Added gnome scaling on work/laptop screens.

---
+++ zenbook/laptop_screen.sh
@@ -0,0 +1,3 @@
+xrandr --output HDMI1 --off
+gsettings set org.gnome.desktop.interface scaling-factor 1
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.5
--- zenbook/work_screen.sh
+++ zenbook/work_screen.sh
@@ -2,4 +2,6 @@ xrandr --newmode "2560x1440" 220.812 2560 2608 2640 2720 1440 1443 1448 1478 -hs
 xrandr --addmode HDMI1 "2560x1440"
 xrandr --output HDMI1 --off
 sleep 2s
-xrandr --output HDMI1 --mode "2560x1440" --left-of eDP1  
+xrandr --output HDMI1 --mode "2560x1440" --right-of eDP1  
+gsettings set org.gnome.desktop.interface scaling-factor 1
+gsettings set org.gnome.desktop.interface text-scaling-factor 1
